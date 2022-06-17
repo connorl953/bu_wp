a1f8b1574c7a78ce03cf6d9cee998b11f051b819
Tanek Swain <87674996+T-S123@users.noreply.github.com>
2022-06-16 21:36:09 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -244,26 +244,3 @@ echo "    </div>\n";
 echo "\n";
 echo "\n";
 ?>
-
- <script src="libs/jquery/jquery.js"></script>
-     <script>
-        $( document ).ready(function() {
-            $("#profile-btn").on('click', () => {
-                $("#profile-section").toggleClass("hidden");
-            });
-
-            $("#u-points-btn").on('click', () => {
-                $("#gift-card-section").toggleClass("hidden");
-                
-            });
-
-            $("#donation-btn").on('click', () => {
-                $("#donation-field").toggleClass("hidden");
-            });
-
-            $("#history-btn").on('click', () => {
-                 $("#table-of-history").toggleClass("hidden");
-             });
-    });
-
-    </script>
