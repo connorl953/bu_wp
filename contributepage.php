fb6e8c2c57d2886b6c82679fdf87afeb5ef20652
Tanek Swain <87674996+T-S123@users.noreply.github.com>
2022-06-16 21:26:30 -0400

    Update contributepage.php

--- contributepage.php
+++ contributepage.php
@@ -319,18 +319,12 @@ echo "</div>\n";
 echo "\n";
 echo "\n";
 ?>
-<script
-  "<script crossorigin=\"anonymous\"\n";
- "        integrity=\"sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=\"\n";
- "        src=\"https://code.jquery.com/jquery-3.6.0.min.js\"></script>\n";
-"<script crossorigin=\"anonymous\"\n";
-"        integrity=\"sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p\"\n";
- "        src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js\"></script>\n";
- "<script crossorigin=\"anonymous\"\n";
- "        integrity=\"sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF\"\n";
- "        src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js\"></script>\n";
- "</body>\n";
- "\n";
- "</html>";
-</script>
-?>
+<script crossorigin="anonymous"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script crossorigin="anonymous"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script crossorigin="anonymous"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
